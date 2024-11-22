caedda509bd512d0063a4b7b0cd8db4e7b15404e
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2024-11-22 16:14:41 -0300

    Create exercicio1.c
    Uma das piores soluções já feitas para esse programa

---
+++ aula 22-11/exercicio1.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *resultado = malloc(sizeof(int) *2);
+    
+    
+    for(int contador1 = 0; contador1<numsSize; contador1++){
+        for(int contador2 = 1; contador2<numsSize; contador2++){
+            if( nums[contador1] + nums[contador2] == target && contador1 != contador2){
+                resultado[0] = contador1;
+                resultado[1] = contador2;
+                *returnSize = 2;
+                return resultado;
+            }
+            
+        }
+        
+    }
+
+        return 0;
+
+    }
+ 
+
+    
+// UMA DAS PIORES SOLUÇÕES JÁ FEITAS
